b10c120e5eb7748b83898ce1135cd8200dbea860
darken <darken@darken.eu>
2022-07-16 22:19:50 +0200

    Fix apk path for CI release job

--- buildSrc/src/main/java/ProjectConfig.kt
+++ buildSrc/src/main/java/ProjectConfig.kt
@@ -11,7 +11,7 @@ object ProjectConfig {
     object Version {
         const val major = 0
         const val minor = 0
-        const val patch = 6
+        const val patch = 7
         const val build = 0
 
         const val name = "${major}.${minor}.${patch}"
