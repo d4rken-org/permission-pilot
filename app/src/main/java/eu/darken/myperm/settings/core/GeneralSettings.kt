faec448969c91962408c793f0f2f5b8e019175cf
darken <darken@darken.eu>
2022-07-26 03:54:21 +0200

    Move bugsnag toggle to support screen, and fix duplicate preference being referenced.

--- app/src/main/java/eu/darken/myperm/settings/core/GeneralSettings.kt
+++ app/src/main/java/eu/darken/myperm/settings/core/GeneralSettings.kt
@@ -7,6 +7,7 @@ import com.squareup.moshi.Moshi
 import dagger.hilt.android.qualifiers.ApplicationContext
 import eu.darken.myperm.apps.ui.list.AppsFilterOptions
 import eu.darken.myperm.apps.ui.list.AppsSortOptions
+import eu.darken.myperm.common.debug.autoreport.DebugSettings
 import eu.darken.myperm.common.debug.logging.logTag
 import eu.darken.myperm.common.preferences.*
 import eu.darken.myperm.permissions.ui.list.PermsFilterOptions
@@ -18,12 +19,11 @@ import javax.inject.Singleton
 class GeneralSettings @Inject constructor(
     @ApplicationContext private val context: Context,
     private val moshi: Moshi,
+    private val debugSettings: DebugSettings,
 
     ) : Settings() {
 
     override val preferences: SharedPreferences = context.getSharedPreferences("settings_core", Context.MODE_PRIVATE)
-
-    val isBugTrackingEnabled = preferences.createFlowPreference("core.bugtracking.enabled", true)
 
     val appsFilterOptions = preferences.createFlowPreference(
         "apps.list.options.filter",
@@ -48,7 +48,7 @@ class GeneralSettings @Inject constructor(
     )
 
     override val preferenceDataStore: PreferenceDataStore = PreferenceStoreMapper(
-        isBugTrackingEnabled
+        debugSettings.isAutoReportingEnabled
     )
 
     companion object {
