e01355a0d68c517b7bb4c4f970cfe615666f288f
darken <darken@darken.eu>
2022-07-30 14:02:11 +0200

    Version bump (0.6.0)

--- buildSrc/src/main/java/ProjectConfig.kt
+++ buildSrc/src/main/java/ProjectConfig.kt
@@ -13,8 +13,8 @@ object ProjectConfig {
 
     object Version {
         const val major = 0
-        const val minor = 5
-        const val patch = 7
+        const val minor = 6
+        const val patch = 0
         const val build = 0
 
         const val name = "${major}.${minor}.${patch}"
