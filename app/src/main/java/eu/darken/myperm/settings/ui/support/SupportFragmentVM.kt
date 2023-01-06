29c0366e8e0cf5274f5af0d7325ed9d59faf70d0
darken <darken@darken.eu>
2023-01-06 11:07:26 +0100

    Remove superfluous debug options such as the install ID (we don't have bugsnag activated).

--- app/src/main/java/eu/darken/myperm/settings/ui/support/SupportFragmentVM.kt
+++ app/src/main/java/eu/darken/myperm/settings/ui/support/SupportFragmentVM.kt
@@ -1,34 +1,20 @@
 package eu.darken.myperm.settings.ui.support
 
-import android.content.Intent
 import androidx.lifecycle.SavedStateHandle
 import dagger.hilt.android.lifecycle.HiltViewModel
-import eu.darken.myperm.common.EmailTool
-import eu.darken.myperm.common.InstallId
 import eu.darken.myperm.common.coroutine.DispatcherProvider
 import eu.darken.myperm.common.debug.logging.log
 import eu.darken.myperm.common.debug.recording.core.RecorderModule
-import eu.darken.myperm.common.livedata.SingleLiveEvent
 import eu.darken.myperm.common.uix.ViewModel3
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 @HiltViewModel
 class SupportFragmentVM @Inject constructor(
-    private val handle: SavedStateHandle,
-    private val emailTool: EmailTool,
-    private val installId: InstallId,
-    private val dispatcherProvider: DispatcherProvider,
+    @Suppress("unused") private val handle: SavedStateHandle,
+    dispatcherProvider: DispatcherProvider,
     private val recorderModule: RecorderModule,
 ) : ViewModel3(dispatcherProvider) {
-
-    val emailEvent = SingleLiveEvent<Intent>()
-    val clipboardEvent = SingleLiveEvent<String>()
-
-    fun copyInstallID() = launch {
-        log { "copyInstallID()" }
-        clipboardEvent.postValue(installId.id)
-    }
 
     val isRecording = recorderModule.state.map { it.isRecording }.asLiveData2()
 
