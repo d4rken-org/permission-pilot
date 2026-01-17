8efcbaca660c1544bdbff56af0d199591b22db22
darken <darken@darken.eu>
2026-01-17 14:06:42 +0100

    Update AGP to 8.13.2 and fix deprecation warnings
    - Upgrade Android Gradle Plugin from 8.11.0 to 8.13.2
    - Fix exec(String) deprecation by using array-based exec
    - Remove unused Packaging function and import
    - Move targetSdk from defaultConfig to testOptions/lint for libraries
    - Add CLAUDE.md project documentation

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,7 +8,7 @@ repositories {
     mavenCentral()
 }
 dependencies {
-    implementation("com.android.tools.build:gradle:8.11.0")
+    implementation("com.android.tools.build:gradle:8.13.2")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.24")
     implementation("com.squareup:javapoet:1.13.0")
 }
