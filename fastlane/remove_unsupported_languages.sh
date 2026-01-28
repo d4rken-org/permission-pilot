c82098c41e723d66d25c5e631f61f96eab2e6386
darken <darken@darken.eu>
2026-01-28 09:45:14 +0000

    Add Crowdin CLI and configuration for translations
    - Add crowdin.yml with project configuration and language mappings
    - Add crowdin.sh helper script for CLI usage
    - Add crowdin-cli.jar for local translation management
    - Add Crowdin to acknowledgements screen
    - Update fastlane to exclude additional unsupported languages (kaa, or-IN, nb)

--- fastlane/remove_unsupported_languages.sh
+++ fastlane/remove_unsupported_languages.sh
@@ -14,5 +14,8 @@ rm -rv ./metadata/android/ur-IN
 rm -rv ./metadata/android/zu
 rm -rv ./metadata/android/si-LK
 rm -rv ./metadata/android/pa-IN
+rm -rv ./metadata/android/kaa
+rm -rv ./metadata/android/or-IN
+rm -rv ./metadata/android/nb
 find ./metadata/android -empty -type d -delete
 exit 0
