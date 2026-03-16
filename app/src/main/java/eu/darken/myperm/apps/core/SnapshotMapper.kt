8e3591959d9d3d2058ec888ed3438c1985122695
darken <darken@darken.eu>
2026-03-16 22:16:50 +0100

    Fix device admin filter to check requestedPermissions instead of receivers only

--- app/src/main/java/eu/darken/myperm/apps/core/SnapshotMapper.kt
+++ app/src/main/java/eu/darken/myperm/apps/core/SnapshotMapper.kt
@@ -8,6 +8,7 @@ import eu.darken.myperm.apps.core.container.SecondaryProfilePkg
 import eu.darken.myperm.apps.core.container.SecondaryUserPkg
 import eu.darken.myperm.apps.core.container.UninstalledDataPkg
 import eu.darken.myperm.apps.core.features.UsesPermission
+import eu.darken.myperm.permissions.core.known.APerm
 import eu.darken.myperm.common.room.entity.PkgType
 import eu.darken.myperm.common.room.entity.SnapshotPkgDeclaredPermEntity
 import eu.darken.myperm.common.room.entity.SnapshotPkgEntity
@@ -49,7 +50,7 @@ class SnapshotMapper @Inject constructor(
             twinCount = pkg.twins.size,
             siblingCount = pkg.siblings.size,
             hasAccessibilityServices = pkg.accessibilityServices.isNotEmpty(),
-            hasDeviceAdmin = pkg.deviceAdmins.any { it.isActive },
+            hasDeviceAdmin = pkg.requestedPermissions.any { it.id == APerm.BIND_DEVICE_ADMIN.id },
             allInstallerPkgNames = pkg.installerInfo.allInstallers
                 .map { it.id.pkgName }
                 .takeIf { it.isNotEmpty() }
